f31751c8a15f1a38485a2a44833d7b75351426a5
Ivan Bojovic <bojova281279@gmail.com>
2022-06-09 13:33:03 +0200

    connexion au MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 // Importation express.
 const express = require('express');
 
+// Importation mongoose.
+const mongoose = require('mongoose');
+
+// DOTENV pour gérer les variables d'environnements.
+require('dotenv').config();
+
+// Connexion à la base de données.
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n8duq.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 app.use((req, res, next) => {
