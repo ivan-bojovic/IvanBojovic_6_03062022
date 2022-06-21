fc965981558138f3ada7939ed43f221f75d58749
Ivan Bojovic <bojova281279@gmail.com>
2022-06-21 10:21:14 +0200

    changement nom fichier images

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 
+// vérification du format des images ajoutées et modification de leur nom lors de la sauvegarde
 const  MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
@@ -10,7 +11,7 @@ const storage = multer.diskStorage({
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const name = (file.originalname.split('.')[file.originalname.split('.'). length -1]);
         const extension =  MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' +  extension);
     }
